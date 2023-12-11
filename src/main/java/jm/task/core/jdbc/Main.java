e78bf8e115b4010ad56b0a397240fb147c2eb022
Error <ya.gaukler@gmail.com>
2023-12-11 18:46:21 +0300

    Main has been fixed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,25 +12,42 @@ public class Main {
         // реализуйте алгоритм здесь
         UserDao userDao = new UserDaoJDBCImpl();
 
+        // Создание таблицы
         userDao.createUsersTable();
         System.out.println("Таблица пользователей создана");
 
-        User user1 = new User("John", "Doe", (byte) 25);
+        // Добавляем
+        User user1 = new User("Artem", "2033", (byte) 25);
         userDao.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
         System.out.println("Пользователь с именем " + user1.getName() + " добавлен в базу данных");
 
+        User user2 = new User("Tom", "Cat", (byte) 30);
+        userDao.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("Пользователь с именем " + user2.getName() + " добавлен в базу данных");
 
+        User user3 = new User("Mike", "Johnson", (byte) 35);
+        userDao.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("Пользователь с именем " + user3.getName() + " добавлен в базу данных");
+
+        User user4 = new User("Ivan", "Ivanov", (byte) 40);
+        userDao.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("Пользователь с именем " + user4.getName() + " добавлен в базу данных");
+
+        // Получаем
         List<User> userList = userDao.getAllUsers();
         System.out.println("Список всех пользователей:");
         for (User user : userList) {
             System.out.println(user.toString());
         }
 
-
+        // Очищаем
         userDao.cleanUsersTable();
         System.out.println("Таблица пользователей очищена");
+        System.out.flush();
 
+        // Удаляем
         userDao.dropUsersTable();
         System.out.println("Таблица пользователей удалена");
+        System.out.flush();
     }
 }
